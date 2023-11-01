a48d4c3df7157ca22546cf54eb81bf597a9d1eeb
UTSAV-PC\UTSAV KHAMAR <utsav.khamar@emgage.work>
2023-11-01 14:33:36 +0530

    first(list view) Page

---
+++ lib/models/listDataModel.dart
@@ -0,0 +1,8 @@
+
+class ListViewData {
+  String title, url, description;
+  int UserId;
+
+  ListViewData(
+      {required this.title, required this.url, required this.UserId, required this.description});
+}
--- lib/view/list_view.dart
+++ lib/view/list_view.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:utsav_cloudrevels_prec/services/state_services.dart';
+
+import '../services/Utils/app_urls.dart';
 
 class ListViewScreen extends StatefulWidget {
   const ListViewScreen({super.key});
@@ -8,8 +14,56 @@ class ListViewScreen extends StatefulWidget {
 }
 
 class _ListViewScreenState extends State<ListViewScreen> {
+  List<ListViewData> listdata = [];
+
+  Future<List<ListViewData>> getList() async {
+    final response = await http.get(Uri.parse(AppUrls.listApi));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      listdata.clear();
+      for (Map i in data) {
+        ListViewData listViewData = ListViewData(
+            title: i['title'],
+            url: i['photo_url'],
+            UserId: i['user_id'],
+            description: i['description']);
+        listdata.add(listViewData);
+      }
+      return listdata;
+    } else {
+      return listdata;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 2,
+          title: Text('Home List'),
+        ),
+        body: SafeArea(
+            child: Column(children: [
+          Expanded(
+              child: FutureBuilder(
+            future: getList(),
+            builder: (context, AsyncSnapshot<List<ListViewData>> snapshot) {
+              return ListView.builder(
+                  itemCount: listdata.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            snapshot.data![index].url.toString(),
+                            scale: 2),
+                      ),
+                      title: Text(snapshot.data![index].title.toString()),
+                      subtitle:
+                          Text(snapshot.data![index].description.toString()),
+                    );
+                  });
+            },
+          ))
+        ])));
   }
 }
